9062c7436ca542d72c307aa3ebe70ee9ddb04b77
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-12-28 15:00:57 +0530

    addnew

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import { Router } from 'react-router-dom';
+import './App.css';
+import Parent from './Component/Function-props/Parent.js'
+function App() {
+  return (
+    <div>
+    <Parent/>
+    </div>
+  );
+}
+
+export default App;
